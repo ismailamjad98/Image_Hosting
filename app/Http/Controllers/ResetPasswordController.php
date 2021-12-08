dd2a74a85facf2396bb31d705c3b3a4d29ba28cc
ismail <ismailamjad98@yahoo.com>
2021-12-08 17:43:37 +0500

    third commit

--- app/Http/Controllers/ResetPasswordController.php
+++ app/Http/Controllers/ResetPasswordController.php
@@ -3,17 +3,13 @@
 namespace App\Http\Controllers;
 
 use App\Mail\ForgetPasswordMail;
-use App\Models\ResetPassword;
 use App\Models\User;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Hash;
 use Throwable;
 
 use Firebase\JWT\JWT;
-use App\Mail\Sendmail;
-use App\Models\PasswordForget;
 use App\Models\PasswordReset;
-use App\Models\ResetPasswordModel;
 use App\Services\createToken;
 use Illuminate\Support\Facades\Mail;
 
@@ -47,7 +43,6 @@ class ResetPasswordController extends Controller
             $reset_url = 'http://127.0.0.1:8000/api/reset_password/' . $forgetpass_token . '/' . $request->email;
 
             $user = User::where('email', $request->email)->first();
-
             if (!$user) {
                 return response(['Message' => 'No User Found']);
             }
@@ -74,23 +69,20 @@ class ResetPasswordController extends Controller
                 'password' => 'required|min:8'
             ]);
 
-            $getuser = PasswordReset::where('token', $token)->where('email', $email)->where('expire' , '1')->first();
+            $getuser = PasswordReset::where('token', $token)->where('email', $email)->where('expire', '1')->first();
             // dd($getuser);
-            if($getuser == null){
+            if ($getuser == null) {
                 return response(['Message' => "The token is expire please Try again"]);
             }
-
             if ($getuser) {
                 PasswordReset::where('email', $email)->where('token', $token)->update([
                     'expire' => 0,
                 ]);
-
                 User::where('email', $email)->update([
                     'password' => Hash::make($request->password),
                 ]);
                 return response(['Message' => "Password changed successfully!"]);
             }
-            
         } catch (Throwable $e) {
             return $e->getMessage();
         }
